c5ff8de7e7870f6f306158e471ae97e72a1a6199
Mahmoudfad <you@example.commahmoudfadhel2@gmail.com>
2021-05-18 14:08:00 +0100

    register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register/register.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  loginForm;
+  registerForm; 
+
+  constructor(public router:Router, public snackBar: MatSnackBar, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', Validators.compose([Validators.required])),
+      password: new FormControl('', Validators.compose([Validators.required, Validators.minLength(6)]))
+    });
+    this.registerForm = new FormGroup({
+      name: new FormControl('',Validators.compose([Validators.required, Validators.minLength(3)]) ) ,
+      email:new FormControl('',Validators.compose([Validators.required])),
+      password: new FormControl('', Validators.required),
+      confirmPassword :new FormControl('', Validators.required) 
+    })
+  }
+
+  public onLoginFormSubmit(values:Object):void {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe((res:any)=>{
+       if(JSON.parse(JSON.stringify(res)).message=='Auth failed'){
+        this.snackBar.open('verify your password or email', '×', { panelClass: 'warn', verticalPosition: 'top', duration: 3000 });
+       
+       }
+       else{
+        localStorage.setItem('token',JSON.stringify(res.token));
+       
+        
+        this.router.navigateByUrl('/')
+       
+       }
+        
+      
+      })
+    }
+   
+  }
+
+
+
+  public onRegisterFormSubmit(values:Object):void {
+    if (this.registerForm.valid && this.registerForm.controls.password.value==this.registerForm.controls.confirmPassword.value) {
+      this.authService.register(this.registerForm.value).subscribe((res)=>{
+        this.snackBar.open('You registered successfully!', '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
+      })
+     
+    }
+    else {
+      this.snackBar.open("Password and confirm password don't match!", '×', { panelClass: 'success', verticalPosition: 'top', duration: 3000 });
+    }
+  }
+
+}
